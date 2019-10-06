6d76ced109357ac62ef2abca8e7c880eac5ca54d
John Fernandes <john@Johns-MacBook-Air.local>
2019-10-05 23:51:19 -0400

    adds wget

--- brew.sh
+++ brew.sh
@@ -1,5 +1,3 @@
-
-
 #!/usr/bin/env bash
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&
@@ -9,7 +7,11 @@ brew tap homebrew/completions
 brew install homebrew/completions/docker-completion
 brew install homebrew/completions/docker-compose-completion
 
+# caskk programs
 brew tap caskroom/cask
 brew cask install spotify
 
+# terminal programs
 brew install zsh
+brew install wget
+
