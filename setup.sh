d06c114b1bffc2bf46fbcd86848e79e9cce70485
John <john1625b@gmail.com>
2019-10-08 19:14:23 -0400

    removes zsh install from setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,3 @@
 ln -sv ~/repos/dotfiles/.bashrc ~
 ln -sv ~/repos/dotfiles/.vimrc ~
 ln -sv ~/repos/dotfiles/.zshrc ~
-
-
-# Installs non-OS specific programs
-
-# oh-my-zsh 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
-chsh -s $(which zsh) 
-
