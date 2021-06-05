98929d3825d5a8d00d552cad00e6d3af85b9ecd8
John <john1625b@gmail.com>
2021-06-05 08:00:51 -0400

    adds git config with aliases

--- symlinking.sh
+++ symlinking.sh
@@ -5,3 +5,4 @@ ln -svnf ~/repos/dotfiles/.bashrc ~
 ln -svnf ~/repos/dotfiles/.vimrc ~
 ln -svnf ~/repos/dotfiles/.ideavimrc ~
 ln -svnf ~/repos/dotfiles/.zshrc ~
+ln -svnf ~/repos/dotfiles/.gitconfig ~
