1dfba9d522977fc98f7414e14cb069edfb48199d
John Fernandes <john1625b@gmail.com>
2021-06-07 22:38:38 -0400

    adds npmrc dotfile

--- symlinking.sh
+++ symlinking.sh
@@ -6,3 +6,4 @@ ln -svnf ~/repos/dotfiles/.vimrc ~
 ln -svnf ~/repos/dotfiles/.ideavimrc ~
 ln -svnf ~/repos/dotfiles/.zshrc ~
 ln -svnf ~/repos/dotfiles/.gitconfig ~
+ln -svnf ~/repos/dotfiles/.npmrc ~
